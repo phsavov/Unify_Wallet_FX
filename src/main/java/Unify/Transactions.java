f1fb4f1486626d65db5ddc736a9a1445df38009c
SerratedGraph77 <serratedgraph74@gmail.com>
2021-11-23 19:28:28 -0600

    Update Transactions.java

--- src/main/java/Unify/Transactions.java
+++ src/main/java/Unify/Transactions.java
@@ -1,5 +1,10 @@
 package Unify;
 
+/*
+This is in charge of the address and amounts sent and received from a user to another user.
+-This information is used to construct the Ledger in the MainPageController.
+*/
+
 public class Transactions {
 
     private String fromAccountID;
@@ -12,12 +17,15 @@ public class Transactions {
      * @param amount
      * @param toAccountID
      */
+    //Constructor
     public Transactions(String fromAccountID, String amount, String toAccountID){
         this.fromAccountID = fromAccountID;
         this.amount = amount;
         this.toAccountID = toAccountID;
     }
 
+    
+    //Getters
     public String getFromAccountID() {
         return fromAccountID;
     }
